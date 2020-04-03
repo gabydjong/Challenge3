c0b59d7789a62b26b74cd5ec0685c3b4aac5bc23
Gaby de Jong <gabydejong@mbp-van-gaby.home>
2020-04-03 17:55:55 +0200

    veelaanpassingen

--- js/main.js
+++ js/main.js
@@ -62,6 +62,113 @@ function onAPIError(error) {
 	 document.getElementById('button').classList.add('btn-animation'); 
 }
 
-/* MAPS google */
+/* MAPS */
+//Dit is het code voor de MAP
+function showMapBox(){
+	mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FieTkiLCJhIjoiY2s4azUwZXJvMDIwMDNtb296ejRzdmtxciJ9.ai4IrKbKLtQWdU_IGAd-Kw';
+	var map = new mapboxgl.Map({
+		container: 'map', // container id
+		style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+		center: [50, 50], // starting position [lng, lat]
+		zoom: 1 // starting zoom
+	});
+
+// Zoekfunctie
+var geocoder = new MapboxGeocoder({
+	accessToken: mapboxgl.accessToken,
+	marker: {
+		color: '#2C3F51'
+	},
+	flyTo: {
+		bearing: 0,
+		// These options control the flight curve, making it move
+		// slowly and zoom out almost completely before starting
+		// to pan.
+		speed: 0.4, // make the flying slow
+		curve: 1, // change the speed at which it zooms out
+		// This can be any easing function: it takes a number between
+		// 0 and 1 and returns another number between 0 and 1.
+		easing: function(t) {
+			return t;
+		}
+	},
+	mapboxgl: mapboxgl
+});
+
+// Geschikte landingsplekken 
+var geojson = {
+	'type': 'FeatureCollection',
+	'features': [
+		{
+			'type': 'Feature',
+			'properties': {
+				'message': 'Foo',
+				'iconSize': [60, 60]
+			},
+			'geometry': {
+				'type': 'Point',
+				'coordinates': [-66.324462890625, -16.024695711685304]
+			}
+		},
+		{
+			'type': 'Feature',
+			'properties': {
+				'message': 'Bar',
+				'iconSize': [50, 50]
+			},
+			'geometry': {
+			'type': 'Point',
+				'coordinates': [-61.2158203125, -15.97189158092897]
+			}
+		},
+		{
+			'type': 'Feature',
+			'properties': {
+				'message': 'Baz',
+				'iconSize': [40, 40]
+			},
+			'geometry': {
+			'type': 'Point',
+				'coordinates': [-63.29223632812499, -18.28151823530889]
+			}
+		}
+	]
+};
+
+// add markers to map 
+geojson.features.forEach(function(marker) {
+	// create a DOM element for the marker
+	var el = document.createElement('div');
+	el.className = 'marker';
+	el.style.backgroundImage =
+		'url(https://placekitten.com/g/' +
+		marker.properties.iconSize.join('/') +
+		'/)';
+	el.style.width = marker.properties.iconSize[0] + 'px';
+	el.style.height = marker.properties.iconSize[1] + 'px';
+	 
+	el.addEventListener('click', function() {
+		window.alert(marker.properties.message);
+});
+ 
+// add marker to map
+new mapboxgl.Marker(el)
+.setLngLat(marker.geometry.coordinates)
+.addTo(map);
+});
+
+// zoekbalk
+map.addControl(geocoder);
+
+// Navigatie buttons
+map.addControl(new mapboxgl.NavigationControl());
+
+// optie fullscreen
+map.addControl(new mapboxgl.FullscreenControl());
+
+}
 
 
+window.onload = function() {
+  showMapBox();
+}
